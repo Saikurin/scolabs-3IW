e00790129241fd3b14cbafb0a51153a9c0fe2b55
Yves skrzypczyk <skrzypczyk@MacBook-Pro-de-Yves.local>
2019-11-19 14:27:38 +0100

    init cache

---
+++ www/cache/routes.cache.php
@@ -0,0 +1,27 @@
+<?php array (
+  '/' => 
+  array (
+    'controller' => 'default',
+    'action' => 'default',
+  ),
+  '/test/yves' => 
+  array (
+    'controller' => 'default',
+    'action' => 'default',
+  ),
+  '/ajout-utilisateur' => 
+  array (
+    'controller' => 'user',
+    'action' => 'add',
+  ),
+  '/utilisateur' => 
+  array (
+    'controller' => 'user',
+    'action' => 'default',
+  ),
+  '/connexion' => 
+  array (
+    'controller' => 'user',
+    'action' => 'login',
+  ),
+)
---
+++ www/core/helpers.class.php
@@ -0,0 +1,14 @@
+<?php
+class helpers{
+
+
+	public static function getUrl($controller, $action){
+
+		
+
+
+		return $url
+	}
+
+
+}
